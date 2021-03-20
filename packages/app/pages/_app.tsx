1fecc21c9a5ae0a852600b1ef867cafdd9ecc0e9
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2021-03-20 15:21:11 +1100

    Add scores UI

---
+++ packages/app/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "../styles/global.css";
+import "antd/dist/antd.css";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
