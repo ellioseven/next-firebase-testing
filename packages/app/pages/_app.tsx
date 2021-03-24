a8bb9a4ecfac64f2178bdc55ffe81661ab74a455
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2021-03-25 08:52:47 +1100

    Add missing types

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/global.css";
 import "antd/dist/antd.css";
+import { AppProps } from "next/app";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
