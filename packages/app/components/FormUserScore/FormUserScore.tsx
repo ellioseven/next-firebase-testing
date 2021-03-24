ae9919a51cfd01ba1bbfcab343f2fd4bb27d2ce5
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2021-03-25 08:54:30 +1100

    Update

--- packages/app/components/FormUserScore/FormUserScore.tsx
+++ packages/app/components/FormUserScore/FormUserScore.tsx
@@ -21,7 +21,7 @@ const tailLayout = {
 const rules = [{ required: true, message: "Required" }];
 
 // @todo Add loading state.
-// @todo Clearn form state after successful submit.
+// @todo Clean form state after successful submit.
 export const FormUserScore = () => {
   const handleFinish = async (values: any) => {
     fetch("http://localhost:3000/api/score/create", {
