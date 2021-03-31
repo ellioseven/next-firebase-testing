a0b77fd5e7ba74365bf223d4a3d911931ce39f4b
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2021-03-31 21:07:28 +1100

    Add firebase config to jest etup

--- packages/app/jest.node.setup.ts
+++ packages/app/jest.node.setup.ts
@@ -2,3 +2,13 @@ import "@testing-library/jest-dom";
 import fetch from "isomorphic-unfetch";
 
 global.fetch = fetch;
+
+const projectId = process.env.GCLOUD_PROJECT;
+
+// Config is manually set to prevent:
+// "Warning, estimating Firebase Config based on GCLOUD_PROJECT. Initializing firebase-admin may fail"
+process.env.FIREBASE_CONFIG = JSON.stringify({
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  storageBucket: `https://${projectId}.appspot.com`,
+  projectId,
+});
